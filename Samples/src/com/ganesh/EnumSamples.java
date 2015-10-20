5ca63a640bf88ec1a4491a2be4d24652ea5db09c
Ganesh Sekaran <ganesh.sekaran@gmail.com>
2015-10-20 07:46:41 +0530

    changed

--- Samples/src/com/ganesh/EnumSamples.java
+++ Samples/src/com/ganesh/EnumSamples.java
@@ -1,22 +1,63 @@
 package com.ganesh;
 
-public class EnumSamples {
-	
-	public enum Singleton{
+public class EnumSamples implements Cloneable {
+
+	public static void main(String[] args) throws Exception{
+		EnumSamples main = new EnumSamples();
+		main.test();
+		EnumSamples cloned = (EnumSamples) main.clone();
+		cloned.test();
+	}
+
+	public enum Singleton {
 		INSTATNCE;
-		
+
 		private Singleton() {
 			// TODO Auto-generated constructor stub
 		}
-		
-		public static final Singleton getInstance(){
+
+		public static final Singleton getInstance() {
 			return INSTATNCE;
 		}
 	}
-	
-	
-	public void test(){
-		Singleton tetst = 	Singleton.getInstance();
+
+	public enum AbstractTest implements Runnable {
+		TEST {
+			void type() {
+
+			}
+
+			public void run() {
+				super.run();
+				System.out.println("Override run");
+
+			}
+			
+			@Override
+			protected void testOveride() {
+				// TODO Auto-generated method stub
+				super.testOveride();
+			}
+
+		};
+		abstract void type();
+
+		public void run() {
+			System.out.println("Run");
+		}
+		
+		protected void testOveride(){
+			
+		}
+	}
+
+	public void test() {
+		Singleton tetst = Singleton.getInstance();
+
+		Thread t = new Thread(AbstractTest.TEST);
+		t.start();
+		
+		System.out.println(AbstractTest.TEST);
 	}
 
 }
